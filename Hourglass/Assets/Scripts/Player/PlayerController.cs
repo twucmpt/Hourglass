6161dacf60ff074635d202000328d0994250a7cc
Brady C <bwjccoles@gmail.com>
2019-04-26 22:57:49 -0700

    Added player movement and basic object "physics".
    Mostly copy/pasted from https://unity3d.com/learn/tutorials/topics/2d-game-creation/scripting-collision?playlist=17093.
    Floors need colliders, player and objects need colliders and RigidBody2D set to Kinematic.

---
+++ Hourglass/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Modified from https://unity3d.com/learn/tutorials/topics/2d-game-creation/scripting-gravity?playlist=17093
+
+public class PlayerController : PhysicsObject
+{
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+    private SpriteRenderer spriteRenderer;
+    //private Animator animator;
+
+    // Use this for initialization
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        //animator = GetComponent<Animator>();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        // An animator can be used for changing sprites and such, walk cycles etc.
+        /* 
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+        */ 
+
+        targetVelocity = move * maxSpeed;
+    }
+}

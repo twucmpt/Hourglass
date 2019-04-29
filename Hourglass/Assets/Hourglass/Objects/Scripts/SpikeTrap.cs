d768643207ddc006f97871d64fcaa45e45a62804
SamDelaney <sam@thedelaneys.net>
2019-04-29 09:01:48 -0700

    File corrections

---
+++ Hourglass/Assets/Hourglass/Objects/Scripts/SpikeTrap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Hourglass.Characters;
+
+public class SpikeTrap : MonoBehaviour
+{
+    double timeUp = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy")
+        {
+            Character victim = collision.gameObject.GetComponent<Character>();
+            victim.Damage(25);
+        }
+    }
+}
---
+++ Hourglass/Assets/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Hourglass;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        GameManager.NextLevel();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ShowCredits()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
